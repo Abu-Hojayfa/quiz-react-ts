35dac1b0dec6311026dfbedd424d8c3c2c5eebaa
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-07-26 22:34:22 +0600

    add questions

--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -1,21 +1,72 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
 
 interface userData {
-
     name: string;
     category: string;
     difficulty: string;
     amount: string;
-
 }
+
+interface allQuestions {
+  question: string;
+  category: string;
+  correct_answers: string;
+  incorrect_answers: string;
+  difficulty: string;
+} 
 
 const Question = ({name, category, difficulty, amount}:userData) => {
 
-  console.log(name)
+  const [isCorrectAns, setIsCorrectAns] = useState({ca:0})
+  const [isNegAns, setIsNegAns] = useState({na:0})
+  const [questions, setQuestions] = useState<allQuestions[]>([])
+
+  useEffect(()=>{
+    fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`)
+    .then(res => res.json())
+    .then(data => setQuestions(data.results));
+  }, [name, category, difficulty, amount])
+
+  console.log(questions)
 
   return (
     <div>
-      <h1>{name}</h1>
+      <div className="introForm m-auto">
+        <div>
+        <Swiper
+          pagination={{
+            type: "progressbar"
+          }}
+          navigation={true}
+          cssMode={true}
+          className="mySwiper"
+        >
+          {
+            questions.map((question)=>
+              <SwiperSlide>
+                <h1>{question.question}</h1>
+              </SwiperSlide>
+            )
+          }
+          
+        </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
