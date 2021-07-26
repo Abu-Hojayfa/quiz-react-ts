3449afd4d080739b5aad58e4f30500cf90c8615e
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-07-26 20:10:25 +0600

    intro form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import './App.css';
 import { useForm } from 'react-hook-form';
 import Question from './Components/Question';
-import LoadImg from '../src/image/load.gif';
+import LoadImg from '../src/image/load2.gif';
+import top from '../src/image/loading.gif';
 
 interface AppProps {
   name: string;
@@ -35,38 +36,60 @@ function App() {
       <>
         {
           isWindowLoad === true ?
-          <div className="App">{isFormFill === false ? <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" required placeholder="Enter your Name" {...register("name", { required: true, maxLength: 80 })} />
-          <select {...register("category", { required: true })}>
-            <option value="9">General Knowledge</option>
-            <option value="23">History</option>
-            <option value="22">Geography</option>
-            <option value="30">Science: Gadgets</option>
-            <option value="17">Science & Nature</option>
-            <option value="19">Science: Mathematics</option>
-            <option value="18">Science: Computers</option>
-            <option value="15">Entertainment: Video Games</option>
-            <option value="25">Arts</option>
-            <option value="28">Vehicles</option>
-            <option value="21">Sports</option>
-            <option value="10">Entertainment: Book</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="32">Entertainment: Cartoon & Animations</option>
-            <option value="31">Entertainment: Japanese Anime & Manga</option>
-          </select>
-          <select value="okk" {...register("difficulty", { required: true })}>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-          <select {...register("amount", { required: true })}>
-            <option value="10">10</option>
-            <option value="12">12</option>
-            <option value="15">15</option>
-          </select>
-  
-          <input type="submit" />
-        </form> : <Question />}</div>
+          <div className="App">
+            {isFormFill === false ? 
+              <form className="introForm m-auto row g-3" onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="w-25 introPic m-auto">
+                  <img className=" img-fluid" src={top} alt="" />
+                </div>
+
+              <input className="form-control m-2" type="text" required placeholder="Enter your Name" {...register("name", { required: true, maxLength: 80 })} />
+
+
+              <div className="col-md-5">
+                <label htmlFor="inputCity" className="form-label">Category</label>
+                <select  className="form-select" {...register("category", { required: true })}>
+                <option value="9">General Knowledge</option>
+                <option value="23">History</option>
+                <option value="22">Geography</option>
+                <option value="30">Science: Gadgets</option>
+                <option value="17">Science & Nature</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="18">Science: Computers</option>
+                <option value="15">Entertainment: Video Games</option>
+                <option value="25">Arts</option>
+                <option value="28">Vehicles</option>
+                <option value="21">Sports</option>
+                <option value="10">Entertainment: Book</option>
+                <option value="11">Entertainment: Film</option>
+                <option value="32">Entertainment: Cartoon & Animations</option>
+                <option value="31">Entertainment: Japanese Anime & Manga</option>
+              </select>
+              </div>
+              
+              <div className="col-md-4">
+                <label htmlFor="inputState" className="form-label">Difficulty Mode</label>
+                <select className="form-select" {...register("difficulty", { required: true })}>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+              </div>
+
+              <div className="col-md-3">
+                <label htmlFor="inputZip" className="form-label">Questions</label>
+                <select className="form-select" {...register("amount", { required: true })}>
+                <option value="10">10</option>
+                <option value="12">12</option>
+                <option value="15">15</option>
+              </select>
+              </div>
+      
+              <input className="btn btn-success btn-lg w-25 m-2" type="submit" />
+            </form> : 
+            <Question />}
+          </div>
         : 
       
             <div className=" position-absolute top-50 start-50 translate-middle">
@@ -75,7 +98,6 @@ function App() {
               Loading ...
             </h2>
           </div>
-       
         }
       </>
   );
