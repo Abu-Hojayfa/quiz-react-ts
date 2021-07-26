33a3d0ac6f2b609e94022a5b1e350112c61ff349
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-07-26 17:15:36 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useForm } from 'react-hook-form';
+import Question from './Components/Question';
+import LoadImg from '../src/image/load.gif';
+
+interface AppProps {
+  name: string;
+  category: string;
+  difficulty: string;
+  amount: string;
+}
 
 function App() {
+
+  const [userData, setUserData] = useState({} as AppProps);
+
+  const [isFormFill, setIsFormFill] = useState(false)
+  const [isWindowLoad, setWindowLoad] = useState(true)
+
+  const { register, handleSubmit, } = useForm();
+  const onSubmit = (data: AppProps) => {
+    setUserData(data);
+    setIsFormFill(true);
+    console.log(userData)
+  };
+
+  window.onload = () => {
+    setWindowLoad(false)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        {
+          isWindowLoad === true ?
+          <div className="App">{isFormFill === false ? <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" required placeholder="Enter your Name" {...register("name", { required: true, maxLength: 80 })} />
+          <select {...register("category", { required: true })}>
+            <option value="9">General Knowledge</option>
+            <option value="23">History</option>
+            <option value="22">Geography</option>
+            <option value="30">Science: Gadgets</option>
+            <option value="17">Science & Nature</option>
+            <option value="19">Science: Mathematics</option>
+            <option value="18">Science: Computers</option>
+            <option value="15">Entertainment: Video Games</option>
+            <option value="25">Arts</option>
+            <option value="28">Vehicles</option>
+            <option value="21">Sports</option>
+            <option value="10">Entertainment: Book</option>
+            <option value="11">Entertainment: Film</option>
+            <option value="32">Entertainment: Cartoon & Animations</option>
+            <option value="31">Entertainment: Japanese Anime & Manga</option>
+          </select>
+          <select value="okk" {...register("difficulty", { required: true })}>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <select {...register("amount", { required: true })}>
+            <option value="10">10</option>
+            <option value="12">12</option>
+            <option value="15">15</option>
+          </select>
+  
+          <input type="submit" />
+        </form> : <Question />}</div>
+        : 
+      
+            <div className=" position-absolute top-50 start-50 translate-middle">
+            <img className="img-fluid" src={LoadImg} alt="" />
+            <h2 className="text-center loading">
+              Loading ...
+            </h2>
+          </div>
+       
+        }
+      </>
   );
 }
 
---
+++ src/Components/Question.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Question = () => {
+  return (
+    <div>
+      <h1>Okay</h1>
+    </div>
+  );
+};
+
+export default Question;
